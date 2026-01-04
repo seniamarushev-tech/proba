479b53cd1b6963f5532f7883c5dc701522863321
seniamarushev-tech <seniamarushev@gmail.com>
2026-01-05 01:52:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // === ВСТАВЬ СЮДА СВОИ ДАННЫЕ SUPABASE ===
 window.TRUST_CONFIG = {
-  SUPABASE_URL: "https://kqriapcsyuqyuxaulamp.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxcmlhcGNzeXVxeXV4YXVsYW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1NDc5MjksImV4cCI6MjA4MzEyMzkyOX0.DQ4DFuQB33w9-pnYHlniRhSWUTkTImi2qSQNxFK29_A ",
+  SUPABASE_URL: https://kqriapcsyuqyuxaulamp.supabase.co
+  SUPABASE_ANON_KEY: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtxcmlhcGNzeXVxeXV4YXVsYW1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1NDc5MjksImV4cCI6MjA4MzEyMzkyOX0.DQ4DFuQB33w9-pnYHlniRhSWUTkTImi2qSQNxFK29_A ,
 
   // bucket name в Supabase Storage (у тебя он называется "Demo")
   DEMO_BUCKET: "Demo",
